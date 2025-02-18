1efeaf66c17f33534c0ab21070665e4be71ca33d
riper03 <115179321+riper03@users.noreply.github.com>
2025-02-17 18:15:04 -0600

    Delete HeladosExamen directory

--- HeladosExamen/app.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js', {scope: './'})
-        .then(function(registration){
-            console.log('Services Worker registrdo!!', registration);
-        })
-        .catch(function(error) {
-            console.log('Error al registrar el service worker', error);
-        });
-    }
