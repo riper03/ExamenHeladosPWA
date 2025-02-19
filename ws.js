820513210c970906e04ea9bdb1370cd29b96aee9
riper03 <115179321+riper03@users.noreply.github.com>
2025-02-18 21:21:08 -0600

    Add files via upload

--- ws.js
+++ ws.js
@@ -1,37 +1,37 @@
 var CACHE_NAME = 'v3Helados';
 var cacheFiles = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/manifest.json',
-    '/contacto.html',
-    '/productos.html',
-    '/ubicacion.html',
-    '/nosotros.html',
-    '/img/Carrusel1.jpg',
-    '/img/Carrusel2.jpg',
-    '/img/Carrusel3.jpg',
-    '/img/Carrusel4.jpeg',
-    '/img/CubetasHelados.jpeg',
-    '/img/Desktop_Captura.png',
-    '/img/EdoMexMapa.jpeg',
-    '/img/ESTi.jpg',
-    '/img/fondo.jpg',
-    '/img/HeladosSabores.jpg',
-    '/img/Huasca.jpg',
-    '/img/icono1.png',
-    '/img/icono2.png',
-    '/img/MexicoBandera.png',
-    '/img/Mobile_Captura.png',
-    '/img/PachucaMapa.png',
-    '/img/PaletasHielo.jpg',
-    '/img/Perfil.png',
-    '/img/Temascalapa.jpeg',
-    '/img/TizayucaMapa.png',
-    '/img/VillasTezontepecMapa.png',
-    '/img/mision.png',
-    '/img/vision.jpg',
-    '/img/objetivos.jpg',
+    './',
+    './index.html',
+    './style.css',
+    './manifest.json',
+    './contacto.html',
+    './productos.html',
+    './ubicacion.html',
+    './nosotros.html',
+    './img/Carrusel1.jpg',
+    './img/Carrusel2.jpg',
+    './img/Carrusel3.jpg',
+    './img/Carrusel4.jpeg',
+    './img/CubetasHelados.jpeg',
+    './img/Desktop_Captura.png',
+    './img/EdoMexMapa.jpeg',
+    './img/ESTi.jpg',
+    './img/fondo.jpg',
+    './img/HeladosSabores.jpg',
+    './img/Huasca.jpg',
+    './img/icono1.png',
+    './img/icono2.png',
+    './img/MexicoBandera.png',
+    './img/Mobile_Captura.png',
+    './img/PachucaMapa.png',
+    './img/PaletasHielo.jpg',
+    './img/Perfil.png',
+    './img/Temascalapa.jpeg',
+    './img/TizayucaMapa.png',
+    './img/VillasTezontepecMapa.png',
+    './img/mision.png',
+    './img/vision.jpg',
+    './img/objetivos.jpg',
 ]
 
 self.addEventListener('install', function(e) {
@@ -75,42 +75,19 @@ self.addEventListener('activate', (event) => {
     });
 });
 
-// Fetch
+//fetch
 self.addEventListener('fetch', (event) => {
     console.log('Service Worker: Fetch solicitado para', event.request.url);
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
-                // Si encontramos la respuesta en la caché, la devolvemos
                 if (response) {
                     return response;
                 }
-
-                // Si no está en caché, intentamos hacer un fetch a la red
-                return fetch(event.request)
-                    .then((networkResponse) => {
-                        // Si la respuesta de la red es válida, la retornamos
-                        if (networkResponse) {
-                            return networkResponse;
-                        }
-
-                        // Si la respuesta de la red es inválida, devolvemos una respuesta vacía
-                        return new Response('Error en la solicitud de red', {
-                            status: 500,
-                            statusText: 'Error de red'
-                        });
-                    })
-                    .catch((error) => {
-                        // Si hubo un error al hacer fetch (por ejemplo, no hay conexión a Internet),
-                        // devolvemos una respuesta de fallback desde la caché o una respuesta personalizada
-                        console.error('Error en la solicitud fetch', error);
-                        return new Response('No se pudo acceder a la red', {
-                            status: 503,
-                            statusText: 'Sin conexión a Internet'
-                        });
-                    });
+                return fetch(event.request);
             })
-    );
+            .catch((error) => console.error('Error en la solicitud fetch', error))
+    );
 });
 
 
